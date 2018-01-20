47c59abe79b6a8ae6f0b297647cf179fa995d05f
jhsc <jsantacruz13@gmail.com>
2018-01-19 17:52:52 -0800

    Added default values

--- cors.go
+++ cors.go
@@ -2,13 +2,32 @@ package cors
 
 import (
 	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	defaultAllowHeaders = []string{"Origin", "Accept", "Content-Type", "Authorization"}
+	defaultAllowMethods = []string{"GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"}
 )
 
 // Options ...
 type Options struct {
-	AllowOrigins  []string
-	AllowMethods  []string
-	AllowHeaders  []string
-	ExposeHeaders []string
-	MaxAge        time.Duration
+	AllowOrigins     []string
+	AllowMethods     []string
+	AllowHeaders     []string
+	ExposeHeaders    []string
+	AllowCredentials bool
+	MaxAge           time.Duration
+}
+
+// Middleware sets CORS headers for every request
+func Middleware(opts Options) gin.HandlerFunc {
+	if opts.AllowHeaders == nil {
+		opts.AllowHeaders = defaultAllowHeaders
+	}
+
+	if opts.AllowMethods == nil {
+		opts.AllowMethods = defaultAllowMethods
+	}
 }
