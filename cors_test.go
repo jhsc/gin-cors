65ba1cb448b269fc457d5d94a76ace955021f28c
jhsc <jsantacruz13@gmail.com>
2018-01-25 23:40:15 -0800

    Strated test package

--- cors_test.go
+++ cors_test.go
@@ -1,1 +1,27 @@
 package cors
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	gin.SetMode(gin.TestMode)
+}
+
+func newTestRouter(opts Options) *gin.Engine {
+	router := gin.New()
+	router.Use(Middleware(opts))
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "get")
+	})
+	router.POST("/", func(c *gin.Context) {
+		c.String(200, "post")
+	})
+	router.PUT("/", func(c *gin.Context) {
+		c.String(200, "put")
+	})
+	router.PATCH("/", func(c *gin.Context) {
+		c.String(200, "patch")
+	})
+	return router
+}
